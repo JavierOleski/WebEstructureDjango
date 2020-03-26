99844bd7b30f48074b2c91f9efecb5c3d9a306c5
JavierOleski <oleski.villanueva@gmail.com>
2020-03-26 11:56:12 -0500

    Estructura web-pruebaHome

--- django_WebEstructure/urls.py
+++ django_WebEstructure/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django_WebEstructure.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', home_view, name='home'),
+    path('', home_view, name='home')
 ]
